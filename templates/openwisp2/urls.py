d3802e699662cc9525dd659885cb4f871ca3a9d8
Federico Capoano <nemesis@ninux.org>
2018-02-19 16:37:04 +0100

    [django] Added support for django 2.0

--- templates/openwisp2/urls.py
+++ templates/openwisp2/urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 from openwisp_utils.admin_theme.admin import admin, openwisp_admin
+
+try:
+    from django.urls import reverse_lazy
+except ImportError:
+    from django.core.urlresolvers import reverse_lazy
 
 openwisp_admin()
 
 redirect_view = RedirectView.as_view(url=reverse_lazy('admin:index'))
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('openwisp_controller.urls')),
     {% if openwisp2_network_topology %}
     url(r'^', include('openwisp_network_topology.urls')),
