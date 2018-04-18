openwisp/ansible-openwisp2

commit 1ca800d25c2a0b2cd6793c4a5fab778383fe7980

[django_cache] Adds cache templating settings #78
Made changes to settings.py in templates/openwisp2 from the references
mentioned to enable django templating using django redis.
Added variable openwisp2_redis_location in defaults/main.yml for django redis

Fixes Issue #78

templates/openwisp2/settings.py
@@ -106,11 +106,10 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
-            'loaders': [
+            'loaders': ('django.template.loaders.cached.Loader', [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-                'openwisp_utils.loaders.DependencyLoader'
-            ],
+            ]),
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -120,6 +119,21 @@ TEMPLATES = [
         },
     },
 ]
+
+# FOR DJANGO REDIS
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "{{ openwisp2_redis_location }}",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 

defaults/main.yml
@@ -70,3 +70,5 @@ openwisp2_nginx_ssl_config:
         - "application/xml"
         - "application/x-font-ttf"
         - "font/opentype"
+
+openwisp2_redis_location: redis://127.0.0.1:6379/1

commit c663de21a965c4df121befd52b1395d0c94ca764

[django] Fixed TEMPLATES setting
Fixed bug introduced in 1ca800d

templates/openwisp2/settings.py
@@ -106,10 +106,13 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
-            'loaders': ('django.template.loaders.cached.Loader', [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ]),
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                    'openwisp_utils.loaders.DependencyLoader'
+                ]),
+            ],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',