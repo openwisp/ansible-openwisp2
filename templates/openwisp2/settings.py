8bea8f2f02a1b9b879d79da425ae6ba86fb0ffc5
purhan <purhan01@gmail.com>
2021-03-23 14:07:07 +0530

    [change] Add influxdb role and suggested changes

--- templates/openwisp2/settings.py
+++ templates/openwisp2/settings.py
@@ -59,11 +59,6 @@ INSTALLED_APPS = [
 {% if openwisp2_firmware_upgrader %}
     'openwisp_firmware_upgrader',
 {% endif %}
-{% if openwisp2_monitoring %}
-    'openwisp_monitoring.monitoring',
-    'openwisp_monitoring.device',
-    'openwisp_monitoring.check',
-{% endif %}
     # openwisp2 admin theme
     # (must be loaded here)
     'openwisp_utils.admin_theme',
@@ -471,13 +466,13 @@ EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 
 {% endfor %}
 
-{% if openwisp2_influxdb_install %}
+{% if openwisp2_monitoring %}
 TIMESERIES_DATABASE = {
     'BACKEND': '{{ openwisp2_timeseries_database.backend }}',
     'USER': '{{ openwisp2_timeseries_database.user }}',
     'PASSWORD': '{{ openwisp2_timeseries_database.password }}',
     'NAME': '{{ openwisp2_timeseries_database.name }}',
-    'HOST': '{{ openwisp2_timeseries_database.host }}',
-    'PORT': '{{ openwisp2_timeseries_database.port }}',
+    'HOST': '{{ influxdb_http_ip }}',
+    'PORT': '{{ influxdb_http_port }}',
 }
 {% endif %}
