6f164cb6905823f36fb7b45353836497fc55e116
Gagan Deep <pandafy.dev@gmail.com>
2021-12-17 20:19:06 +0530

    [change] Renamed custom static directory to ow2_static #322
    Related to #322

--- templates/openwisp2/settings.py
+++ templates/openwisp2/settings.py
@@ -338,7 +338,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static-custom')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ow2_static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
