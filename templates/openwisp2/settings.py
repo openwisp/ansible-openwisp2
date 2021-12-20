204c7a6082d300a70b923ae387ca86cec58af357
Gagan Deep <pandafy.dev@gmail.com>
2021-12-20 18:06:42 +0530

    [requested-changes] Changed remote static directory name

--- templates/openwisp2/settings.py
+++ templates/openwisp2/settings.py
@@ -341,7 +341,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ow2_static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_custom')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
