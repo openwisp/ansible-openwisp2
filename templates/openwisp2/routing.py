b2c6431e6a8e1eb1e83fa77d18fba3af00ca4d19
Gagan Deep <pandafy.dev@gmail.com>
2022-09-13 20:45:13 +0530

    [feature] Made websocket routes configurable

--- templates/openwisp2/routing.py
+++ templates/openwisp2/routing.py
@@ -2,12 +2,25 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from openwisp_controller.routing import get_routes
+{% for import in openwisp2_websocket_extra_imports %}
+{{ import }}
+{% endfor %}
+
+routes = []
+
+{% if openwisp2_controller_urls %}
+from openwisp_controller.routing import get_routes as get_controller_routes
+routes.extend(get_controller_routes())
+{% endif %}
+
+{% for extra_routes in openwisp2_websocket_extra_routes %}
+routes.extend({{ extra_routes }})
+{% endfor %}
 
 application = ProtocolTypeRouter(
     {
         'websocket': AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(get_routes()))
+            AuthMiddlewareStack(URLRouter(routes))
         )
     }
 )
