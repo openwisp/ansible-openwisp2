b22f7b990807e0fde8021f87b840823a7ffe849b
Gagan Deep <pandafy.dev@gmail.com>
2024-11-27 22:53:38 +0530

    [fix] Fixed outdated __openwisp_version__

--- templates/openwisp2/__init__.py
+++ templates/openwisp2/__init__.py
@@ -1,7 +1,7 @@
 from .celery import app as celery_app
 
 __all__ = ['celery_app']
-__openwisp_version__ = '24.0.0a'
+__openwisp_version__ = '24.11.1'
 __openwisp_installation_method__ = (
     '{{ openwisp2_installation_method | default("ansible-openwisp2") }}'
 )
