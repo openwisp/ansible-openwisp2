8d27da05381eef351327c5ddaee19f9bee0c0dbf
Federico Capoano <f.capoano@openwisp.io>
2024-12-27 12:30:00 +0100

    [fix] Separate version to avoid circular import
    This patch allows importing the version from settings.py
    without suffering exceptions which are caused by circular
    imports.
    
    (cherry picked from commit 65f2497324e1d3c2b568ebf36339eb800b1f3211)

--- templates/openwisp2/__init__.py
+++ templates/openwisp2/__init__.py
@@ -1,7 +1,8 @@
 from .celery import app as celery_app
+from .version import __openwisp_version__, __openwisp_installation_method__
 
-__all__ = ['celery_app']
-__openwisp_version__ = '24.11.1'
-__openwisp_installation_method__ = (
-    '{{ openwisp2_installation_method | default("ansible-openwisp2") }}'
-)
+__all__ = [
+    'celery_app',
+    '__openwisp_version__',
+    '__openwisp_installation_method__'
+]
