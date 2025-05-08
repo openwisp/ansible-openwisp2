2aca8395288d01cc70465ec4902b5576e5d4f190
Gagan Deep <pandafy.dev@gmail.com>
2025-05-08 13:56:30 -0300

    [deps] Bumped Django~=5.2.0 and development version of OpenWISP #513
    Closes #513
    
    Co-authored-by: Federico Capoano <f.capoano@openwisp.io>

--- templates/openwisp2/routing.py
+++ templates/openwisp2/routing.py
@@ -1,6 +1,7 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
 {% for import in openwisp2_websocket_extra_imports %}
 {{ import }}
@@ -29,6 +30,7 @@ application = ProtocolTypeRouter(
     {
         'websocket': AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(routes))
-        )
+        ),
+        'http': get_asgi_application(),
     }
 )
