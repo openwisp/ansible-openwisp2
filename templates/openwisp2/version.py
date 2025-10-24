612b8fc95b6ec31393271008d08e61762e0b86a4
Gagan Deep <pandafy.dev@gmail.com>
2025-10-24 21:43:45 +0530

    25.10.0 release
    Signed-off-by: Gagan Deep <pandafy.dev@gmail.com>

--- templates/openwisp2/version.py
+++ templates/openwisp2/version.py
@@ -1,4 +1,4 @@
-__openwisp_version__ = "25.07.0a"
+__openwisp_version__ = "25.10.0"
 __openwisp_installation_method__ = (
     '{{ openwisp2_installation_method | default("ansible-openwisp2") }}'
 )
